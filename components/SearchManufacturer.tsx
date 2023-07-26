96425c4709d2ac7c029568128aeadd60675a2b30
Firda Rahayu <firdarahayu105@gmail.com>
2023-07-26 16:05:13 +0700

    Search Manufacturer

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import React, {useState, Fragment} from 'react'
 import {  manufacturers } from '@/constants'
 
-const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+const SearchManufacturer = ({ selected, setSelected }: SearchManufacturerProps) => {
   const [query, setQuery] = useState('');
 
   const filteredManufactures = query === ""
@@ -19,7 +19,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
   return (
     <div className='search-manufacturer'>
-      <Combobox value={manufacturer} onChange={setManufacturer}>
+      <Combobox value={selected} onChange={setSelected}>
         <div className="relative w-full">
           <Combobox.Button className='absolute top-[14px]'>
             <Image 
