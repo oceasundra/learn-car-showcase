1ebe0928581bbc08bc355de4c4c290166b8db418
Firda Rahayu <firdarahayu105@gmail.com>
2023-07-27 09:54:53 +0700

    Custom Filter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -2,9 +2,7 @@
 
 import { Fragment, useState } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 import { Listbox, Transition } from "@headlessui/react";
-import { updateSearchParams } from "@/utils";
 import { CustomFilterProps } from "@/types";
 
 export default function CustomFilter({ title, options, setFilter }: CustomFilterProps) {
